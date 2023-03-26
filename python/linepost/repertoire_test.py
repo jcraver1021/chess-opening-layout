d1ac2c47299a80aca17162f6e9fe04e735c7a9a5
James Craver <25520616+jcraver1021@users.noreply.github.com>
2023-03-26 16:57:40 -0400

    Add first test

--- python/linepost/repertoire_test.py
+++ python/linepost/repertoire_test.py
@@ -1,1 +1,12 @@
+from parameterized import parameterized
+import linepost.repertoire as repertoire
 
+@parameterized([
+    ('e4', 'e4', []),
+    ('Ke2??', 'Ke2', []),
+    ('Qh4;rude;wtf', 'Qh4', ['rude', 'wtf']),
+])
+def test_parse_move(input_move, expected_label, expected_remarks):
+    move, _, remarks = repertoire.parse_move(input_move)
+    assert expected_label == move
+    assert expected_remarks == remarks
