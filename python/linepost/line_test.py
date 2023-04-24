32d1b770bdeb0f798858e6936abe288ef3e39f32
James Craver <25520616+jcraver1021@users.noreply.github.com>
2023-04-24 10:04:00 -0400

    Test labels and evals in line

--- python/linepost/line_test.py
+++ python/linepost/line_test.py
@@ -1,17 +1,20 @@
 import pytest
 import linepost.line as lpline
 
-@pytest.mark.parametrize("string,want_moves_length,want_remarks_by_index", [
-    ('e4 e5 Nf3', 4, {}),
-    ('e4 e5 Nf3 Nf6', 5, {}),
-    ('', 1, {}),
-    ('e4', 2, {}),
-    ('d4|I better not see another London d5 Bf4|really?!|goddammit', 4, {1: ['I better not see another London'], 3: ['really?!', 'goddammit']}),
+@pytest.mark.parametrize("string,want_labels,want_evals_by_index,want_remarks_by_index", [
+    ('e4 e5 Nf3', ['e4', 'e5', 'Nf3'], {}, {}),
+    ('e4 e5 Nf3 Nc6', ['e4', 'e5', 'Nf3', 'Nc6'], {}, {}),
+    ('', [], {}, {}),
+    ('e4', ['e4'], {}, {}),
+    ('d4|I better not see another London d5 Bf4?!|really?!|goddammit', ['d4', 'd5', 'Bf4'], {3: '?!'}, {1: ['I better not see another London'], 3: ['really?!', 'goddammit']}),
 ])
-def test_lines(string, want_moves_length, want_remarks_by_index):
+def test_lines(string, want_labels, want_evals_by_index, want_remarks_by_index):
     line = lpline.Line(string)
-    assert want_moves_length == len(line.moves)
+    assert len(want_labels) + 1 == len(line.moves)
     for i, move in enumerate(line.moves):
+        if i > 0:
+            assert move.label == want_labels[i - 1]
+            assert move.evaluation == want_evals_by_index.get(i, None)
         assert move.remarks == want_remarks_by_index.get(i, [])
 
 # TODO: Test that invalid moves result in a failure.
