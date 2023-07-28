753a749a1418bdbf58b673aa6c5aa97928351c6f
James Craver <25520616+jcraver1021@users.noreply.github.com>
2023-07-28 16:41:12 -0400

    and newlines

--- python/linepost/repertoire_test.py
+++ python/linepost/repertoire_test.py
@@ -22,6 +22,25 @@ from linepost.repertoire import Repertoire
         '# Some people hate it, but you can play the Alapin if you want',
         'e4 c5 c3',
     ],
+    [
+        'e4 e5 Nf3\n',
+        'e4 c5 Nf3\n',
+    ],
+    [
+        'e4 e5 Nf3\n',
+        '\n',
+        'e4 c5 Nf3\n',
+    ],
+    [
+        'e4 e5 Nf3\n',
+        '    \n',
+        'e4 c5 Nf3\n',
+    ],
+    [
+        'e4 e5 Nf3\n',
+        '# Some people hate it, but you can play the Alapin if you want\n',
+        'e4 c5 c3\n',
+    ],
 ])
 def test_lines(lines):
     _ = Repertoire.from_lines(lines)
