85cd4486778ed2dd97f04a9cbb3b547a86890340
James Craver <25520616+jcraver1021@users.noreply.github.com>
2023-08-29 07:43:23 -0500

    Parse line with move/pos remarks

--- src/linepost/line_test.py
+++ src/linepost/line_test.py
@@ -1,5 +1,7 @@
 import pytest
-from linepost.line import Line, Token, can_parse_line, unlabel_pattern
+
+from linepost.line import (REMARK_POINTER_PATTERN, Line, Token, can_parse_line,
+                           parse_line, unlabel_pattern)
 from linepost.position import Game
 
 
@@ -77,6 +79,10 @@ def test_unlabel_pattern(pattern, unlabeled_pattern):
     ('O-O-O!!', True),
     ('O-O-O-O', False),
     ('chess move', False),
+    ('p17', False),
+    ('P17', False),
+    ('m17', False),
+    ('M17', False),
 ])
 def test_is_chess_move(string, want_bool):
     token = Token(string)
@@ -89,10 +95,25 @@ def test_is_chess_move(string, want_bool):
     ('Nf3', 'Nf3', None),
     ('Nf3!!', 'Nf3', '!!'),
 ])
-def test_tokens(string, want_move, want_eval):
+def test_chess_tokens(string, want_move, want_eval):
     token = Token(string)
     assert want_move == token.get_move()
     assert want_eval == token.get_evaluation()
+
+
+@pytest.mark.parametrize("string,want_remark_type,want_remark_index", [
+    ('great move', None, None),
+    ('Nf3', None, None),
+    ('Nf3!!', None, None),
+    ('p2', 'p', 2),
+    ('P3', 'p', 3),
+    ('m4', 'm', 4),
+    ('M5', 'm', 5),
+])
+def test_remark_tokens(string, want_remark_type, want_remark_index):
+    token = Token(string)
+    assert want_remark_type == token.get_remark_type()
+    assert want_remark_index == token.get_remark_index()
 
 
 @pytest.mark.parametrize(
@@ -112,13 +133,46 @@ def test_can_parse_line(string, want_bool):
 
 
 @pytest.mark.parametrize(
+    "line,want_tokens,want_position_remarks,want_move_remarks", [
+        ('c4? e5!', ['c4?', 'e5!'], [], []),
+        ('c4?        e5!', ['c4?', 'e5!'], [], []),
+        ('e4 m"Be bold!" p"Best by test" p"Black has many responses"', [
+            'e4', 'm0', 'p0', 'p1'
+        ], ['Best by test', 'Black has many responses'], ['Be bold!']),
+    ])
+def test_parse_line(line, want_tokens, want_position_remarks,
+                    want_move_remarks):
+    tokens, position_remarks, move_remarks = parse_line(line)
+    assert want_tokens == list(map(str, tokens))
+    assert want_position_remarks == position_remarks
+    assert want_move_remarks == move_remarks
+
+
+@pytest.mark.parametrize("line", [
+    '',
+    '     ',
+    'Q7',
+    'e4 e5 j7',
+    'this probably does not work',
+    'p"this also should not work"',
+    'm"nor this"',
+])
+def test_parse_line_invalid(line):
+    with pytest.raises(ValueError):
+        _ = parse_line(line)
+
+
+@pytest.mark.parametrize(
     "string,want_labels,want_evals_by_index,want_pos_remarks_by_index,want_move_remarks_by_index",  # noqa: E501
     [
         ('e4 e5 Nf3', ['e4', 'e5', 'Nf3'], {}, {}, {}),
+        ('e4   e5   Nf3', ['e4', 'e5', 'Nf3'], {}, {}, {}),
         ('e4 e5 Nf3 Nc6', ['e4', 'e5', 'Nf3', 'Nc6'], {}, {}, {}),
-        ('', [], {}, {}, {}),
         ('e4', ['e4'], {}, {}, {}),
         ('e4 p"Best by test" p"Black has many responses"', ['e4'], {}, {
+            1: {'Black has many responses', 'Best by test'},
+        }, {}),
+        ('e4    p"Best by test"   p"Black has many responses"', ['e4'], {}, {
             1: {'Black has many responses', 'Best by test'},
         }, {}),
         (
@@ -155,6 +209,8 @@ def test_lines(string, want_labels, want_evals_by_index,
 
 
 @pytest.mark.parametrize("string", [
+    "",
+    "   ",
     "e5",
     "e4 Bc5",
     "e4 e5 O-O O-O",
